331eb6c9319e3cb2aea9dbe6acf8b3c1d23a9204
Rakesh07778777 <bossrakesh43@gmail.com>
2025-12-17 23:53:07 +0530

    elements

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// DOM Elements Cache
+const elements = {
+    navbar: document.querySelector('.navbar'),
+    mobileMenuBtn: document.querySelector('.mobile-menu-btn'),
+    navLinks: document.querySelector('.nav-links'),
+    coachesTrack: document.querySelector('.coaches-track'),
+    sliderDots: document.querySelector('.slider-dots'),
+    prevBtn: document.querySelector('.prev-btn'),
+    nextBtn: document.querySelector('.next-btn')
+};
